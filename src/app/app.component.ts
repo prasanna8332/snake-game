e73a2e8419645bdb700fafb6d9c69564bd1ac2db
prasanna kumar <prasannakumar8332@gmail.com>
2021-06-05 13:34:20 +0530

    improved functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HostListener } from '@angular/core';
+
+export enum KEY_CODE {
+  RIGHT_ARROW = 39,
+  LEFT_ARROW = 37
+}
 
 @Component({
   selector: 'app-root',
@@ -7,34 +13,119 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'snake-game-prasanna';
-  snakeGround: Array<{ turn: boolean; id: number }> = [];
+  groundSize = 130;
+  snakeSpeed = 200;
   headPosition = { turn: true, id: 3 };
+  nextPosition = 0;
+  onGoingGame: any;
+  snakeDirection = "ArrowRight";
+  snakeGround: Array<{ turn: boolean; id: number }> = [];
   snakePositions = [
     { turn: true, id: 0 },
     { turn: true, id: 1 },
     { turn: true, id: 2 },
     { turn: true, id: 3 },
+    { turn: true, id: 4 },
+    { turn: true, id: 5 },
   ];
-  groundSize = 1260;
-  snakeSpeed = 100;
 
   ngOnInit() {
+    this.makeGround();
+    this.makeOutLine();
+  }
+
+  makeOutLine() {
+    console.log("This is for making an outline around the map");
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+
+    if (event.key === "Enter") {
+      this.nextPosition = 1;
+      this.onGoingGame = setInterval(() => { this.startGame() }, this.snakeSpeed);
+      return;
+    }
+    if (event.key === "r") {
+      clearInterval(this.onGoingGame);
+      return;
+    }
+    if (event.key === "ArrowDown") {
+      if (this.snakeDirection === "ArrowUp") return;
+      this.snakeDirection = event.key;
+      this.nextPosition = 13;
+      this.validateSnakeSelfBite();
+      // clearInterval(this.onGoingGame);
+      return;
+    }
+    if (event.key === "ArrowUp") {
+      if (this.snakeDirection === "ArrowDown") return;
+      this.snakeDirection = event.key;
+      this.nextPosition = -13;
+      this.validateSnakeSelfBite();
+      // clearInterval(this.onGoingGame);
+      return;
+    }
+    if (event.key === "ArrowRight") {
+      if (this.snakeDirection === "ArrowLeft") return;
+      this.snakeDirection = event.key;
+      this.nextPosition = 1;
+      this.validateSnakeSelfBite();
+      // clearInterval(this.onGoingGame);
+      return;
+    }
+    if (event.key === "ArrowLeft") {
+      if (this.snakeDirection === "ArrowRight") return;
+      this.snakeDirection = event.key;
+      this.nextPosition = -1;
+      this.validateSnakeSelfBite();
+      // clearInterval(this.onGoingGame);
+      return;
+    }
+  }
+
+  validateSnakeSelfBite() {
+    const position = this.getNextPosition();
+    if(this.snakePositions.map(position => position.id).includes(position.id)) {
+      window.alert("game over da thambi");
+    }
+  }
+
+
+  makeGround(): void {
     for (let i = 0; i < this.groundSize; i++) {
       this.snakeGround.push({ turn: false, id: i });
     }
-    console.log(this.snakeGround);
-    this.snakeGround[this.headPosition.id].turn = true;
-    setInterval(() => {
-      this.startGame();
-    }, this.snakeSpeed)
   }
 
   startGame(): void {
+    const snakePositionsIds = this.calculateTheSnakePositions();
+    this.resetTheGame();
+    this.turnSnakePositionsOn(snakePositionsIds);
+  }
+
+  getNextPosition() {
     const nextPosition = { ...this.headPosition };
-    nextPosition.id = nextPosition.id + 1;
+    nextPosition.id = nextPosition.id + this.nextPosition;
+    return nextPosition;
+  }
+
+  calculateTheSnakePositions(): number[] {
+    const nextPosition = this.getNextPosition();
     this.snakePositions.push(nextPosition);
     this.headPosition = nextPosition;
     this.snakePositions.shift();
+    const snakePositionsIds = this.snakePositions.map((pixel) => pixel.id);
+    return snakePositionsIds;
+  }
+
+  turnSnakePositionsOn(snakePositionsIds: number[]) {
+    this.snakeGround.forEach((pixel) => {
+      pixel.turn = snakePositionsIds.includes(pixel.id)
+    })
+  }
+
+  resetTheGame() {
     if (this.snakePositions[this.snakePositions.length - 1].id === this.snakeGround.length) {
       this.snakePositions = [
         { turn: true, id: 0 },
@@ -44,9 +135,5 @@ export class AppComponent implements OnInit {
       ];
       this.headPosition = { turn: true, id: 3 };
     }
-    const snakePositionsIds = this.snakePositions.map((pixel) => pixel.id);
-    this.snakeGround.forEach((pixel) => {
-      pixel.turn = snakePositionsIds.includes(pixel.id)
-    })
   }
 }
